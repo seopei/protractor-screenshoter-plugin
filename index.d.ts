aaac4b0dd54a04fef0520d7de0f5109a9aa87098
Sebastian Peischl <sebastian.peischl@cnc.io>
2017-01-11 00:21:05 +0100

    feat: update typings

--- index.d.ts
+++ index.d.ts
@@ -3,24 +3,21 @@
 // Definitions by: Sebastian Peischl <https://github.com/seopei>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-declare module ProtractorScreenshoter {
+/**
+ * Add a new browser instance
+ * @param name name of the browser instance
+ * @param skipImageToAscii disable generating ASCII images (optional, default: false)
+ * @returns {*} browser instance
+ */
+export function addScreenshotBrowser(name:string):any;
 
-    /**
-     * Add a new browser instance
-     * @param name name of the browser instance
-     * @param skipImageToAscii disable generating ASCII images (optional, default: false)
-     * @returns {*} browser instance
-     */
-    export function addScreenshotBrowser(name:string):any;
+/**
+ * Remove a browser instance
+ * @param name browser name to remove
+ */
+export function removeScreenshotBrowser(name:string):any;
 
-    /**
-     * Remove a browser instance
-     * @param name browser name to remove
-     */
-    export function removeScreenshotBrowser(name:string):any;
-
-    /**
-     * Remove all browser instances (cleanup)
-     */
-    export function removeAllScreenshotBrowsers():void;
-}
+/**
+ * Remove all browser instances (cleanup)
+ */
+export function removeAllScreenshotBrowsers():void;
