d45e7d73aae742cabfd8353122adbffe85122913
Sebastian Peischl <sebastian.peischl@cnc.io>
2017-01-10 23:54:35 +0100

    feat: update typings

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,26 @@
-export declare function addScreenshotBrowser(name:string, browserInstance:any):void;
-export declare function removeScreenshotBrowser(name:string):void;
+// Type definitions for protractor-screenshoter-plugin
+// Project: https://github.com/azachar/protractor-screenshoter-plugin
+// Definitions by: Sebastian Peischl <https://github.com/seopei>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+declare module ProtractorScreenshoter {
+
+    /**
+     * Add a new browser instance
+     * @param name name of the browser instance
+     * @param skipImageToAscii disable generating ASCII images (optional, default: false)
+     * @returns {*} browser instance
+     */
+    export function addScreenshotBrowser(name:string):any;
+
+    /**
+     * Remove a browser instance
+     * @param name browser name to remove
+     */
+    export function removeScreenshotBrowser(name:string):any;
+
+    /**
+     * Remove all browser instances (cleanup)
+     */
+    export function removeAllScreenshotBrowsers():void;
+}
